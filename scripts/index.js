35922dccd51ae6b754bbd1c4c17503b41a66dac7
samodel <denisenko.vikt@gmail.com>
2023-12-26 08:18:26 +0300

    fix1

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 
 const cardTemplate = document.querySelector('#card-template').content;
-const cardList = document.querySelector('.places__list');
+const cardsContainer = document.querySelector('.places__list');
 
-function addCard (item){
+function createCard (cardData, deleteCard){
   const cardClone = cardTemplate.querySelector('.card').cloneNode(true);
-  const deleteButton = cardClone.querySelector('.card__image');
-  cardClone.querySelector('.card__title').textContent = item.name;
-  deleteButton.src = item.link;
-  deleteButton.alt = item.name;
+  const image = cardClone.querySelector('.card__image');
+  cardClone.querySelector('.card__title').textContent = cardData.name;
+  image.src = cardData.link;
+  image.alt = cardData.name;
 
   const cardDeleteButton = cardClone.querySelector('.card__delete-button');
   cardDeleteButton.addEventListener('click', () => {deleteCard(cardClone);});
@@ -15,12 +15,12 @@ function addCard (item){
   return cardClone;
 }
 
-function deleteCard (item) {
-  item.remove();
+function deleteCard (cardData) {
+  cardData.remove();
 }
 
-initialCards.forEach(item =>{
-  cardList.append(addCard(item, deleteCard));
+initialCards.forEach(cardData =>{
+  cardsContainer.append(createCard(cardData, deleteCard));
 });
 
 
