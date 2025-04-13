f1df6f3c9c8f405ba278ddf738c60da3c45f023b
samodel <denisenko.vikt@gmail.com>
2025-04-13 13:22:57 +0300

    addFnc

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,29 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+  function addCard(item) {
+	const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+	cardElement.querySelector('.card__image').src = item.link;
+	cardElement.querySelector('.card__title').textContent = item.name;
+
+	const deleteButton = cardElement.querySelector('.card__delete-button');
+	deleteButton.addEventListener('click', () => deleteCard(cardElement));
+	return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(cardElement) {
+  cardElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function addCardsToList (cardsArr) {
+	const cards = cardsArr.map(addCard);
+	cards.forEach(card => placesList.append(card));
+}
+
+addCardsToList(initialCards);
